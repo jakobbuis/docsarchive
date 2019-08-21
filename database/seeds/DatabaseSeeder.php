01b848f6d6585ad7edafe15fa4b9c1e9318ddd00
Jakob Buis <jakob.buis@in10.nl>
2019-08-21 19:31:49 +0200

    minimalist seeding

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -1,16 +1,14 @@
 <?php
 
+use App\User;
 use Illuminate\Database\Seeder;
 
 class DatabaseSeeder extends Seeder
 {
-    /**
-     * Seed the application's database.
-     *
-     * @return void
-     */
     public function run()
     {
-        // $this->call(UsersTableSeeder::class);
+        $user = factory(User::class)->create([
+            'email' => 'admin@local',
+        ]);
     }
 }
